8650e3042a99811443d8ab5afb4beaee5ce6ce2b
Matt Zimmer <zimmermja@gmail.com>
2021-11-09 11:55:45 +0000

    Hello World! established

--- app.rb
+++ app.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
-# File initiated
+require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader' if development?
+# http://localhost:4567
+# rackup -p 4567
+
+# This is a default class for testing purposes
+class Default < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello world!'
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
