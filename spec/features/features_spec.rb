0d930010e7a6346d90c13f52d8ae6fc4b9168761
Matt Zimmer <zimmermja@gmail.com>
2021-11-11 12:50:55 +0000

    edited bookmarks to display as names linking to urls

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,9 +6,7 @@ describe BookmarkManager do
   feature 'Viewing bookmarks' do
     scenario 'Presents a list of bookmarks to the user' do
       go_to_bookmarks
-      ['http://www.makersacademy.com',
-       'https://www.codewars.com',
-       'https://www.reddit.com'].each { |url| expect(page).to have_content url }
+      %w[Makers Codewars Reddit].each { |url| expect(page).to have_content url }
     end
   end
 
@@ -16,7 +14,7 @@ describe BookmarkManager do
     scenario 'Completes a form and adds the entered url to the database' do
       add_to_database('Youtube', 'https://www.youtube.com')
       go_to_bookmarks
-      expect(page).to have_content 'Youtube: https://www.youtube.com'
+      expect(page).to have_content 'Youtube'
     end
   end
 end
